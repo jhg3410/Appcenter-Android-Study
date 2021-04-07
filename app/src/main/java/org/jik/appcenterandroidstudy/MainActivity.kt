4e4f71a3c4f17d90d84f76b275e54174a0ee1cbe
jhg3410 <jhg34100@naver.com>
2021-04-07 15:39:45 +0900

    week1

--- app/src/main/java/org/jik/appcenterandroidstudy/MainActivity.kt
+++ app/src/main/java/org/jik/appcenterandroidstudy/MainActivity.kt
@@ -2,10 +2,12 @@ package org.jik.appcenterandroidstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
